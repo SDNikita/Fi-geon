6052a234bb55e18570dd71960d7cccfac488b9d1
SDNikita <sidnevnikitait@gmail.com>
2026-09-23 14:33:10 +0300

    base dots for camera

---
+++ src/services/pose/poseSkeleton.ts
@@ -0,0 +1,25 @@
+export const BODY_LANDMARKS = [
+    11, // left shoulder
+    12, // right shoulder
+
+    13, // left elbow
+    14, // right elbow
+
+    15, // left wrist
+    16, // right wrist
+
+    23, // left hip
+    24, // right hip
+
+    25, // left knee
+    26, // right knee
+
+    27, // left ankle
+    28, // right ankle
+
+    29, // left heel
+    30, // right heel
+
+    31, // left foot index
+    32, // right foot index
+];
